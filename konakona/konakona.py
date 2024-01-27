9042b28101e04df50703868b50f80871a965f154
maxweil <maxweil@notabug.org>
2024-01-27 10:05:47 -0600

    add subtitle burn-in, re-add tree traversal, re-add alternate video directory

--- konakona/konakona.py
+++ konakona/konakona.py
@@ -11,7 +11,7 @@ import tweepy
 def load_config():
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
-    with open(os.path.join(script_dir, 'config.yaml'), 'r') as stream:
+    with open(os.path.join(script_dir, 'testConfig.yaml'), 'r') as stream:
         data = yaml.safe_load(stream)
 
     settings = {
@@ -21,6 +21,7 @@ def load_config():
         'token_secret': data['twitter']['tokenSecret'],
 
         'directory': data['directory'],
+        'video_directory': data['altVidDirectory'],
         'file_ending': data['fileEnding'],
 
         'save': data['save'],
@@ -33,20 +34,36 @@ def load_config():
         'clip_count': data['clip']['clipCount'],
         'clip_seconds_apart': data['clip']['secondsApart'],
         'clip_length': data['clip']['clipLength'],
-
-        'chance_clip': data['chance']['clip']
+        'clip_subtitles':data['clip']['forceSubTrack'],
+        'chance_clip': data['chance']['clip'],
+        'chance_sub': data['chance']['subtitle']
     }
 
     return settings
 
 
 def get_random_filepath(directory, file_ending):
-    file_list = os.listdir(directory)
-    new_file_list = list(filter(lambda x: x.endswith(file_ending), file_list))
+    items = os.listdir(directory)
+    #gather the list of valid files to work on, subdirectories and files of the correct filetype.
+    matching_files = list(filter(lambda x: os.path.isfile(os.path.join(directory, x)) and x.endswith(file_ending), items))
+    subdirectories = list(filter(lambda x: os.path.isdir(os.path.join(directory, x)), items))
 
-    random_file = random.choice(new_file_list)
+    #pull one at random from the combined list    
+    validchoices = matching_files+subdirectories
+    random_selection = random.choice(validchoices)
+    
+    # if we picked a directory, then we should recursively traverse the tree
+    if os.path.isdir(os.path.join(directory, random_selection)):
+        random_file=get_random_filepath(os.path.join(directory, random_selection),file_ending)
+    #otherwise, lets use the file
+    elif os.path.isfile(os.path.join(directory, random_selection)):
+        random_file=random_selection
+    #finally, if there was no valid selection in our list, then quit out. if we were smarter we could probably make this
+    #into a DFS, but i think for now we can say it's user error if there is a dead-end subdirectory.
+    else:
+        return ""
+    
     random_filepath = os.path.join(directory, random_file)
-
     return random_filepath
 
 
@@ -64,7 +81,7 @@ def get_file_length(filepath):
     return float(duration)
 
 
-def generate_screenshot_local(filepath, duration, image_count, seconds_apart):
+def generate_screenshot_local(filepath, duration, image_count, seconds_apart, use_subtitles):
     if image_count >= 5:
         image_count = random.randint(1, 4)
 
@@ -74,14 +91,19 @@ def generate_screenshot_local(filepath, duration, image_count, seconds_apart):
     for i in range(image_count):
         output_name = f"out_{i}.png"
         output_name_list.append(output_name)
-
+    
+    vfString="scale=1920:-1"
+    if use_subtitles:
+        vfString="subtitles='"+filepath+"',"+vfString 
     for i in range(image_count):
         script = [
             'ffmpeg', '-y',
             '-ss', str(random_time),
+            '-copyts',
             '-i', filepath,
+            '-ss', str(random_time),
             '-vframes', '1',
-            '-vf', 'scale=1920:-1',
+            '-vf', vfString,
             '-q:v', '1',
             '-qmin', '1',
             output_name_list[i]
@@ -93,7 +115,7 @@ def generate_screenshot_local(filepath, duration, image_count, seconds_apart):
     return output_name_list
 
 
-def generate_clip_local(filepath, duration, clip_count, seconds_apart, clip_length):
+def generate_clip_local(filepath, duration, clip_count, seconds_apart, clip_length,use_subtitles):
     if clip_count >= 5:
         clip_count = random.randint(1, 4)
 
@@ -104,21 +126,28 @@ def generate_clip_local(filepath, duration, clip_count, seconds_apart, clip_leng
     for i in range(clip_count):
         output_name = f"out_{i}.mp4"
         output_video_list.append(output_name)
-
+    
+    print("use subtitles:"+str(use_subtitles))
+    vfString="scale=1280:-1"
+    if use_subtitles:
+        vfString="subtitles='"+filepath+"',"+vfString 
     for i in range(clip_count):
         script = [
             'ffmpeg', '-y',
             '-ss', str(random_time),
+            '-copyts',
             '-i', filepath,
+            '-ss', str(random_time),
             '-t', str(clip_length),
             '-ac', '2',
             '-sn',
             '-map_chapters', '-1',
             '-c:v', 'libx264',
             '-c:a', 'aac',
-            '-vf', 'scale=1280:-1',
+            '-vf', vfString,
             '-pix_fmt', 'yuv420p',
             '-movflags', '+faststart',
+            '-avoid_negative_ts', 'make_zero',
             output_video_list[i]
         ]
 
@@ -177,16 +206,20 @@ def save_files(clip, output):
 if __name__ == '__main__':
     config = load_config()
 
-    filepath = get_random_filepath(config['directory'], config['file_ending'])
+    generateClip=check_generate(config['chance_clip'])
+    directory = config['video_directory'] if generateClip else config['directory']
+    filepath = get_random_filepath(directory, config['file_ending'])
     duration = get_file_length(filepath)
+    useSubtitles = check_generate(config['chance_sub'])
 
-    if check_generate(config['chance_clip']):
+    if generateClip:
+        if config['clip_subtitles']: useSubtitles=true 
         output = generate_clip_local(filepath, duration, config['clip_count'], config['clip_seconds_apart'],
-                                     config['clip_length'])
+                                     config['clip_length'],useSubtitles)
     else:
-        output = generate_screenshot_local(filepath, duration, config['image_count'], config['image_seconds_apart'])
+        output = generate_screenshot_local(filepath, duration, config['image_count'], config['image_seconds_apart'],useSubtitles)
 
-    post_update(output)
+    #post_update(output)
 
     if config['save']:
         save_files(check_generate(config['chance_clip']), output)
